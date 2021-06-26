e81f9f2d59f5366454315107bbbe045d7ce6c9ba
Arpendu Kumar Garai <Arpendu.KumarGarai@microfocus.com>
2021-06-26 15:51:48 +0530

    modifications

--- node-application-monitoring-app/index.js
+++ node-application-monitoring-app/index.js
@@ -1,6 +1,7 @@
-const http = require('http');
 const url = require('url');
 const client = require('prom-client');
+const express = require('express');
+const app = express();
 const { histogram } = require('./histogram');
 const { gauge } = require('./gauge');
 const { summary } = require('./summary');
@@ -24,7 +25,7 @@ const httpRequestDurationMicroseconds = new client.Histogram({
   help: 'Duration of HTTP requests in seconds',
   labelNames: ['method', 'route', 'code'],
   buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
-})
+});
 
 // Register the histogram
 register.registerMetric(httpRequestDurationMicroseconds);
@@ -54,39 +55,32 @@ const createDelayHandler = async (req, res) => {
   await new Promise(res => setTimeout(res, delaySeconds * 1000))
 
   res.end('Slow url accessed !!');
-}
+};
 
-// Define the HTTP server
-const server = http.createServer(async (req, res) => {
+app.get('/metrics', async (req, res) => {
   // Start the timer
   const end = httpRequestDurationMicroseconds.startTimer();
-  // Retrieve route from request object
   const route = url.parse(req.url).pathname;
 
-  try {
-      if (route === '/metrics') {
-        // Return all metrics the Prometheus exposition format
-        res.setHeader('Content-Type', register.contentType)
-        res.end(register.metrics())
-      }
-
-      if (route === '/slow') {
-        await createDelayHandler(req, res)
-      }
-
-  } catch (error) {
-    res.writeHead(500).end()
-  }
-
-  if (!res.finished) {
-    res.writeHead(404).end()
-  }
+  res.setHeader('Content-Type', register.contentType);
+  res.send(await register.metrics());
 
   // End timer and add labels
-  end({ route, code: res.statusCode, method: req.method })
-})
+  end({ route, code: res.statusCode, method: req.method });
+});
 
-// Start the HTTP server
-server.listen(8080, () => {
+app.get('/slow', async (req, res) => {
+  // Start the timer
+  const end = httpRequestDurationMicroseconds.startTimer();
+  const route = url.parse(req.url).pathname;
+
+  await createDelayHandler(req, res);
+
+  // End timer and add labels
+  end({ route, code: res.statusCode, method: req.method });
+});
+
+// Start the Express server and listen to a port
+app.listen(8080, () => {
   console.log('Server is running on http://localhost:8080, metrics are exposed on http://localhost:8080/metrics')
-})
+});
